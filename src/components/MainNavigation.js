5a2828e74e413c216da7d9438707495b031fb523
jydnbach <jaydenkim95@gmail.com>
2024-06-19 16:52:21 +0900

    root layout

---
+++ src/components/MainNavigation.js
@@ -0,0 +1,22 @@
+import { Link } from 'react-router-dom';
+
+import classes from './MainNavigation.module.css';
+
+function MainNavigation() {
+  return (
+    <header className={classes.header}>
+      <na>
+        <ul className={classes.list}>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/products">Products</Link>
+          </li>
+        </ul>
+      </na>
+    </header>
+  );
+}
+
+export default MainNavigation;
