dc22175703043bc519a615f2e5cf8b79da2d7a52
jydnbach <jaydenkim95@gmail.com>
2024-06-19 16:56:33 +0900

    error page

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ import {
   // Route,
   RouterProvider,
 } from 'react-router-dom';
+
 import HomePage from './pages/Home';
 import ProductsPage from './pages/Products';
 import RootLayout from './pages/Root';
+import ErrorPage from './pages/Error';
 
 // const routeDefinitions = createRoutesFromElements(
 //   <Route>
@@ -19,6 +21,7 @@ const router = createBrowserRouter([
   {
     path: '/',
     element: <RootLayout />,
+    errorElement: <ErrorPage />,
     children: [
       { path: '/', element: <HomePage /> },
       { path: '/products', element: <ProductsPage /> },
