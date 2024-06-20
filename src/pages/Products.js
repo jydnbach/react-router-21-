05198d668ff2db0b5cd7a28893562a299f2c9224
jydnbach <jaydenkim95@gmail.com>
2024-06-20 09:35:05 +0900

    product details page using use params

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,5 +1,14 @@
 function ProductsPage() {
-  return <h1>Products Page</h1>;
+  return (
+    <>
+      <h1>Products Page</h1>
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+      </ul>
+    </>
+  );
 }
 
 export default ProductsPage;
